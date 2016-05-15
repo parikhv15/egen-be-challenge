85a8f81b29daa43567c621956917044eff34bddb
Vraj Parikh <vrajp12@gmail.com>
2016-05-15 12:24:14 -0400

    * Written a test case for metrics read

--- src/test/java/com/egen/EgenBeChallengeApplicationTests.java
+++ src/test/java/com/egen/EgenBeChallengeApplicationTests.java
@@ -1,16 +1,42 @@
 package com.egen;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = EgenBeChallengeApplication.class)
 @WebAppConfiguration
 public class EgenBeChallengeApplicationTests {
 
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+				.build();
+	}
+
+	@Test
+	public void validate_read() {
+		try {
+			mockMvc.perform(get("/metrics/read")).andExpect(status().isOk());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	@Test
 	public void contextLoads() {
 	}
